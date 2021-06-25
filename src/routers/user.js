7fdda5a315e17841bb0fc5cb5b8981022bc3dc3b
furqan_ashraf <furqan_ashraf@outlook.com>
2021-06-25 15:10:10 +0500

    delete consolog

--- src/routers/user.js
+++ src/routers/user.js
@@ -98,8 +98,6 @@ router.get("/users/beneficiaryList", auth, async (req, res) => {
 });
 router.delete("/users/beneficiary/:beneficiary", auth, async (req, res) => {
   // console.log(req);decodeURIComponent('JavaScript_%D1%88%D0%B5%D0%BB%D0%BB%D1%8B')
-  console.log(req.params);
-  console.log(decodeURIComponent(req.params.beneficiary));
   const beneficiary = decodeURIComponent(req.params.beneficiary);
   try {
     let removeIndex = req.user.beneficiaries
